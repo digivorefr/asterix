fd2fce58619d6e88365a60192c45fb3b4d76a69a
Digivore <hello@digivore.fr>
2022-02-04 16:02:17 +0000

    Refacto, add playground, fixes

---
+++ playground/server.js
@@ -0,0 +1,23 @@
+const fs = require('fs-extra');
+const path = require('path');
+const express = require('express');
+
+const mode = process.env.ENV;
+const port = process.env.PORT;
+
+
+const server = express();
+
+console.log('MODE', mode);
+
+// Serves all static assets.
+server.use('/styles', express.static('playground/compiled-css'));
+
+// Catch-all to redirect any request to the main entry point (index.html).
+server.get('/', (_request, response, next) => {
+  response.sendFile(path.join(__dirname, '/index.html'));
+});
+
+server.listen(port, () => {
+  console.log(`Server is up. Port : ${port}`);
+});
